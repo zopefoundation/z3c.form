e804c65378aa8785234101de6b6963f2569436d4
Daniel Nouri <daniel.nouri@gmail.com>
2007-12-04 15:22:52 +0000

    Add a hidden widget template for the ``ISelectWidget``.

--- src/z3c/form/testing.py
+++ src/z3c/form/testing.py
@@ -121,6 +121,10 @@ def setupFormDefaults():
         widget.WidgetTemplateFactory(getPath('select_display.pt'), 'text/html'),
         (None, None, None, None, interfaces.ISelectWidget),
         IPageTemplate, name=interfaces.DISPLAY_MODE)
+    zope.component.provideAdapter(
+        widget.WidgetTemplateFactory(getPath('select_hidden.pt'), 'text/html'),
+        (None, None, None, None, interfaces.ISelectWidget),
+        IPageTemplate, name=interfaces.HIDDEN_MODE)
     # Submit Field Widget
     zope.component.provideAdapter(
         widget.WidgetTemplateFactory(getPath('submit_input.pt'), 'text/html'),

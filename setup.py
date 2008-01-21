f05fd0bb28b19ddac5522eb0ce1dcca825156378
Roger Ineichen <roger@projekt01.ch>
2008-01-21 12:03:15 +0000

    prepare release

--- setup.py
+++ setup.py
@@ -43,7 +43,7 @@ chapters = '\n'.join(
 
 setup (
     name='z3c.form',
-    version='1.7.2dev',
+    version='1.7.2',
     author = "Stephan Richter, Roger Ineichen and the Zope Community",
     author_email = "zope3-dev@zope.org",
     description = "An advanced form and widget framework for Zope 3",

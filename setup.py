e82dab5d79f7ab584b3717d21f115d96d568de26
Vincent Fretin <vincent.fretin@gmail.com>
2009-06-14 09:19:28 +0000

    Removed the escape stuff so the long_description will appear as html on pypi. If it's really needed for something I don't know, please revert.

--- setup.py
+++ setup.py
@@ -20,9 +20,7 @@ import xml.sax.saxutils
 from setuptools import setup, find_packages
 
 def read(*rnames):
-    text = open(os.path.join(os.path.dirname(__file__), *rnames)).read()
-    text = unicode(text, 'utf-8').encode('ascii', 'xmlcharrefreplace')
-    return xml.sax.saxutils.escape(text)
+    return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
 
 chapters = '\n'.join(
     [read('src', 'z3c', 'form', name)

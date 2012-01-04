b7f46b7bf4d5cd03149df01d836bd85dabbc7323
Stephan Richter <stephan.richter@gmail.com>
2012-01-04 13:12:08 +0000

    Changed the official URL to point to Launchpad. I changed the description in Launchpad to contain all other URLs.

--- setup.py
+++ setup.py
@@ -69,7 +69,7 @@ setup(
         'Operating System :: OS Independent',
         'Topic :: Internet :: WWW/HTTP',
         'Framework :: Zope3'],
-    url='http://pypi.python.org/pypi/z3c.form',
+    url='https://launchpad.net/z3c.form',
     packages=find_packages('src'),
     include_package_data=True,
     package_dir={'': 'src'},

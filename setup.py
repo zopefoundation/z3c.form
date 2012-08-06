2e2b1c832c2596b34756f0587dd811fadc68e9b4
Roger Ineichen <roger@projekt01.ch>
2012-08-06 04:52:32 +0000

    added dev marker

--- setup.py
+++ setup.py
@@ -45,7 +45,7 @@ chapters = '\n'.join(
 
 setup(
     name='z3c.form',
-    version='2.8.1',
+    version='2.8.2dev',
     author="Stephan Richter, Roger Ineichen and the Zope Community",
     author_email="zope-dev@zope.org",
     description="An advanced form and widget framework for Zope 3",

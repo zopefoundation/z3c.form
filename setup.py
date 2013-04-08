147811509a8e12186836a7d7aeeebd742efc0c99
Albertas Agejevas <alga@pov.lt>
2013-04-08 17:20:54 +0300

    Preparing release 3.0.0a3

--- setup.py
+++ setup.py
@@ -61,7 +61,7 @@ chapters = '\n'.join(
 
 setup(
     name='z3c.form',
-    version='3.0.0a3.dev0',
+    version='3.0.0a3',
     author="Stephan Richter, Roger Ineichen and the Zope Community",
     author_email="zope-dev@zope.org",
     description="An advanced form and widget framework for Zope 3",

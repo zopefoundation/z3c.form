cd7735fc0b8a53cde45ad09af95ec7bb13b3fdc6
Maurits van Rees <maurits@vanrees.org>
2020-09-21 15:27:22 +0200

    Back to development: 3.7.2
    [ci skip]

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ def alltests():
 
 setup(
     name='z3c.form',
-    version='3.7.1',
+    version='3.7.2.dev0',
     author="Stephan Richter, Roger Ineichen and the Zope Community",
     author_email="zope-dev@zope.org",
     description="An advanced form and widget framework for Zope 3",

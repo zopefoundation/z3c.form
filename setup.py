5fb9780fb7fdc232b6a28a97d79975239c977a47
Michael Howitz <icemac@gmx.net>
2025-04-09 08:40:11 +0200

    Switch to PEP 420 native namespace.

--- setup.py
+++ setup.py
@@ -15,7 +15,6 @@
 """
 import os
 
-from setuptools import find_packages
 from setuptools import setup
 
 
@@ -60,10 +59,7 @@ setup(
         'Framework :: Zope :: 3',
     ],
     url='https://github.com/zopefoundation/z3c.form',
-    packages=find_packages('src'),
     include_package_data=True,
-    package_dir={'': 'src'},
-    namespace_packages=['z3c'],
     python_requires='>=3.9',
     extras_require=dict(
         extra=[

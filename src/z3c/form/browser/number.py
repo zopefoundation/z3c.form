e5c52b5d225c0e0a3967a6040576a51ee48ace08
Johannes Raggam <thetetet@gmail.com>
2025-11-19 12:26:35 +0100

    TMP, DRAFT. Add dedicated number modules.

---
+++ src/z3c/form/browser/number.py
@@ -0,0 +1,47 @@
+##############################################################################
+#
+# Copyright (c) 2007 Zope Foundation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+"""Text Widget Implementation
+
+$Id$
+"""
+__docformat__ = "reStructuredText"
+import zope.component
+import zope.interface
+import zope.schema.interfaces
+
+from z3c.form import interfaces
+from z3c.form.browser import widget
+from z3c.form.widget import FieldWidget
+from z3c.form.widget import Widget
+
+
+@zope.interface.implementer_only(interfaces.ITextWidget)
+class TextWidget(widget.HTMLTextInputWidget, Widget):
+    """Input type text widget implementation."""
+
+    klass = 'text-widget'
+    css = 'text'
+    value = ''
+
+    def update(self):
+        super().update()
+        breakpoint()
+        widget.addFieldClass(self)
+
+
+@zope.component.adapter(zope.schema.interfaces.IField, interfaces.IFormLayer)
+@zope.interface.implementer(interfaces.IFieldWidget)
+def TextFieldWidget(field, request):
+    """IFieldWidget factory for TextWidget."""
+    return FieldWidget(field, TextWidget(request))
